ecc1dc05d0f90e07cb97b3f43d1cc614807e377f
Alaa Gawish <alaagawish33@gmail.com>
2023-05-26 11:48:35 +0300

    launch screen

---
+++ FoodRecipes/Module/Launch/View/LaunchScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LaunchScreenViewController.swift
+//  FoodRecipes
+//
+//  Created by Alaa on 26/05/2023.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+            self.performSegue(withIdentifier: "Home", sender: nil)
+        }
+    }
+
+    
+    
+
+}
