1eb8e78e557389b9b01728396531fab096959a28
Youssef Elbtat <msr.hrh.10@gmail.com>
2023-05-26 13:40:49 +0300

    add design recipe cell

---
+++ FoodRecipes/View/recipeCell.swift
@@ -0,0 +1,45 @@
+//
+//  recipeCell.swift
+//  FoodRecipes
+//
+//  Created by Mac on 26/05/2023.
+//
+
+import UIKit
+
+class recipeCell: UITableViewCell {
+    @IBOutlet weak var imgRecipe: UIImageView!
+    
+    @IBOutlet weak var lblServings: UILabel!
+    @IBOutlet weak var btnFavIcon: UIButton!
+    @IBOutlet weak var lblChefName: UILabel!
+    @IBOutlet weak var lblRecipeName: UILabel!
+    
+    @IBOutlet weak var lblFoodType: UILabel!
+    
+    var isFav = false
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        ViewUtilities.setCornerRadius(view: btnFavIcon, radius: 2.5)
+        // Initializion code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func btnFavAction(_ sender: Any) {
+        if isFav{
+            if let image = UIImage(named: "fillFavIcon") {
+                btnFavIcon.setImage(image, for: .normal)
+            }
+        }else{
+            if let image = UIImage(named: "favicon") {
+                btnFavIcon.setImage(image, for: .normal)
+            }
+        }
+        
+    }
+}
